42c1af607d88029e29cd9a9a927bd63a7f7a1695
piyush260100 <piyush260100@users.noreply.github.com>
2021-04-12 16:27:34 +0530

    Resume Manager

--- app/src/main/java/com/example/rp/HomeActivity.java
+++ app/src/main/java/com/example/rp/HomeActivity.java
@@ -2,12 +2,20 @@ package com.example.rp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import static com.example.rp.R.id.Quiz_Time_buton;
 
@@ -16,7 +24,18 @@ public class HomeActivity extends AppCompatActivity {
     ConstraintLayout resume_view;
     ConstraintLayout quiz_view;
     ConstraintLayout announcement_view;
+    Button editprofile;
+    Button log_out;
 
+    Databasehelper db;
+    DrawerLayout dl;
+    TextView name, email;
+    ImageView image;
+    NavigationView nv;
+    SharedPreferences pref;
+    Button edit, logout;
+
+    private int[] img = {R.drawable.boy, R.drawable.girl1, R.drawable.boyspecs, R.drawable.girl2, R.drawable.man, R.drawable.girl, R.drawable.boynormal, R.drawable.girl3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +44,27 @@ public class HomeActivity extends AppCompatActivity {
 
         resume_view=findViewById(R.id.resume_creator_view);
         announcement_view=findViewById(R.id.announcement_view);
-        quiz_view=findViewById(R.id.announcement_view);
+        quiz_view=findViewById(R.id.quiz_view);
+        editprofile=findViewById(R.id.edit_profile);
+        log_out=findViewById(R.id.logout);
+
+        db = new Databasehelper(this);
+        nv = findViewById(R.id.nav_view);
+        View v = nv.getHeaderView(0);
+        pref = getApplicationContext().getSharedPreferences("mypref", MODE_PRIVATE);
+        name = v.findViewById(R.id.navHeadName);
+        email = v.findViewById(R.id.navHeadEmail);
+        image = v.findViewById(R.id.navHeadImage);
+
+        String ema = pref.getString("KEY_USER", null);
+
+        if (!ema.equals("admin")) {
+            name.setText(db.getName(ema));
+            email.setText(ema);
+            image.setImageResource(img[db.getImagid(ema)]);
+        }
+
+
 
         resume_view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +86,28 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent3 = new Intent(view.getContext(), Announcements.class);
                 view.getContext().startActivity(intent3);}
         });
+
+        editprofile.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent4 = new Intent(view.getContext(), Edit_Profile.class);
+                view.getContext().startActivity(intent4);
+            }
+        });
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor edit = pref.edit();
+                edit.clear();
+                edit.apply();
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
 
     }
