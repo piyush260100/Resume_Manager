9c3045228c64aca42d1e516cc9678e09e9aaab67
piyush260100 <piyush260100@users.noreply.github.com>
2021-04-14 01:41:47 +0530

    Resume Manager

--- app/src/main/java/com/example/rp/ResumeCreator.java
+++ app/src/main/java/com/example/rp/ResumeCreator.java
@@ -3,12 +3,41 @@ package com.example.rp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 
 public class ResumeCreator extends AppCompatActivity {
+
+    View personal_info;
+    View education;
+    View skill;
+    View achievements;
+    View projects;
+    View objective;
+    TextView name,email;
+    ImageView image;;
+    View bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__resume___creator);
+
+        personal_info=findViewById(R.id.personal_details_view);
+        education=findViewById(R.id.education_view);
+        skill=findViewById(R.id.skill_view);
+        achievements=findViewById(R.id.achievement_view);
+        projects=findViewById(R.id.project_view);
+        objective=findViewById(R.id.objective_view);
+        name = findViewById(R.id.navHeadName);
+        email = findViewById(R.id.navHeadEmail);
+        image = findViewById(R.id.navHeadImage);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+
+
+
     }
 }
